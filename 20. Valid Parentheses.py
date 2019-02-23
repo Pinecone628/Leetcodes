84718d4c33f759a931750c07ce5910c59404f4e3
Pinecone628 <aurorawu1996@gmail.com>
2019-02-22 23:09:38 -0600

    New

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: 'str') -> 'bool':
+        mapping = {")": "(", "}": "{", "]": "["}
+        ss = []
+
+        for i in range(len(s)):
+            if s[i] not in mapping:
+                ss.append(s[i])
+            else:
+                if len(ss) == 0:
+                    return False
+                elif mapping[s[i]] != ss[-1]:
+                    return False
+                else:
+                    ss.pop()
+
+        if len(ss) == 0:
+            return True
+        else:
+            return False
